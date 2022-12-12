ef1cc29c5a5cbf0830810224cfa56f9059ec55ae
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2022-12-12 11:00:39 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,8 @@
-@extends('page-content')
-
+<html>
+    <body>
+        @extends('page-content')
+        <div>
+            <img class="bg-img" src="photo/apc-building.png" alt="apc building background">
+        </div>
+    </body>
+    </html>
