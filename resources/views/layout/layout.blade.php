e8ed97b6a103c5af24569da20406af5909575c34
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2022-12-12 10:51:04 +0800

    additional files

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TAPM</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-700 text-white">
+    <header>
+        Header
+    </header>
+    <main>
+        @yield('page-content')
+    </main>
+    <footer>
+        Footer
+    </footer>
+</body>
+</html>
