bea7e85223f76accbe8a166d62cfd0ebf9917f22
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2022-12-17 17:34:04 +0800

    update in layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,8 @@
     <style>
         body{ background-image: url('/storage/apc-building.png');}
     </style>
-    <div class="center">
-        <img src = "/storage/logo.png" class="logo">
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <img src = "/storage/logo.png">
     </div>
 </body>
 @endsection
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,36 +1,70 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TAPM</title>
-    <link rel="stylesheet" href="/css/style.css">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body class="bg-gray-700 text-white">
-    <header class="top">
-        <div id="navbar">
-            <div>
-                <input type="search" name="search" placeholder="Search Something">
-            </div>
-            <div class="right">
-                <a href="{{ route('login') }}">
-                <button class="button">
-                    <strong>LOGIN</strong>
-                </button>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: #164771;">
+            <div class="container">
+                <a class="navbar-brand fw-bold" href="{{ url('/') }}">
+                    TAPM
                 </a>
-                <a href="{{ route('signup') }}">
-                <button class="button">
-                    <strong>SIGN UP</strong>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                </a>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item fw-bold">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('LOGIN') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item fw-bold">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('SIGN UP') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
-        </div>
+        </nav>
     </header>
     <main>
         @yield('page-content')
     </main>
-    <footer class="bottom">
-        Footer
-    </footer>
+    
 </body>
 </html>
