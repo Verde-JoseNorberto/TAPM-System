365b6de28894ca746741ff32cfe67676d3d680aa
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2023-01-06 12:36:04 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-})->name('index');
+    return view('home');
+})->name('/home');
 
 Route::get('/login', function () {
     return view('login');
-})->name('login');
+})->name('/login');
 
 Route::get('/register', function () {
     return view('register');
-})->name('register');
+})->name('/register');
 
 Route::get('/logout', function () {
     return view('logout');
-})->name('logout');
+})->name('/logout');
 
 Auth::routes();
-Route::get('/home', 
-    [App\Http\Controllers\HomeController::class, 'index'
-])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/faculty', [App\Http\Controllers\FacultyController::class, 'index'])->name('faculty');
+Route::get('/client', [App\Http\Controllers\ClientController::class, 'index'])->name('client');
+Auth::routes();
