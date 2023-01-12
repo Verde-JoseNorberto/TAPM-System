5ea9f9e8d11a196aef53a90138a8be9e0b2cabf3
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2023-01-12 20:10:30 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GroupProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,11 @@ Route::get('/', function () {
     return view('auth.login');
 })->name('login');
 
+Route::get('/project', [App\Http\Controllers\GroupProjectController::class, 'index'])->name('faculty/project');
+Route::post('/project', [App\Http\Controllers\GroupProjectController::class, 'store'])->name('faculty/project');
+
 Auth::routes();
-// Route::get('/home', [App\Http\Controllers\StudentController::class, 'index'])->name('student.home');
+// Route::get('/home', [App\Http\Controllers\StudentController::class, 'index'])->name('student/home');
 Route::get('/home', [App\Http\Controllers\FacultyController::class, 'index'])->name('faculty/home');
-// Route::get('/home', [App\Http\Controllers\ClientController::class, 'index'])->name('client.home');
+// Route::get('/home', [App\Http\Controllers\ClientController::class, 'index'])->name('clienthome');
 Auth::routes();
