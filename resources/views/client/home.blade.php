f5bb99df9313d883c45fc52d9d3e52598cfd62d2
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2023-01-23 08:37:36 +0800

    Updated Views

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,40 +1,22 @@
 @extends('layout.layCli')
 
 @section('page-content')
-<div class="container">
-    <div class="row-md-4">
-        <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <td>ID</td>
-                    <td>Title</td>
-                    <td>Category</td>
-                    <td>Year and Term</td>
-                    <td>Due Date</td>
-                    <td>Team</td>
-                    <td>Advisor</td>
-                    <td>Actions</td>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach($group_projects as $groupProject)
-                <tr>
-                    <td>{{ $groupProject->id }}</td>
-                    <td>{{ $groupProject->project_title }}</td>
-                    <td>{{ $groupProject->project_category }}</td>
-                    <td>{{ $groupProject->year_term }}</td>
-                    <td>{{ $groupProject->due_date }}</td>
-                    <td>{{ $groupProject->team }}</td>
-                    <td>{{ $groupProject->advisor }}</td>
-        
-                    <td>       
-                        <a class="btn btn-small btn-success" href="{{ URL::to('project/' . $groupProject->id) }}">Show</a>
-                        
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-        <table>
+<div class="container my-4">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        @foreach($group_projects as $groupProject)
+        <div class="col flex">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h3 class="card-title">{{ $groupProject->project_title }}</h3>
+                    <h6>{{ $groupProject->project_category }}</h6>
+                    <h6>{{ $groupProject->year_term }}</h6>
+                    <h6>{{ $groupProject->team }}</h6>
+                    <h6>{{ $groupProject->advisor }}</h6>
+                    <a class="btn btn-small btn-success streched-link" href="{{ URL::to('project/' . $groupProject->id) }}">Show</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
     </div>
 </div>
 @endsection
--- resources/views/client/project.blade.php
+++ resources/views/client/project.blade.php
@@ -1,33 +1,15 @@
 @extends('layout.layCli')
 
 @section('page-content')
-<div class="container">
+<div class="container my-4">
     <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-right">
-                <a class="btn btn-outline-secondary" href="{{ route('faculty/home') }}"> Back</a>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <br><h2>{{ $group_projects->project_title }}</h2>
+                <strong>Team:</strong> {{ $group_projects->team }}<br>
+                <strong>Advisor:</strong> {{ $group_projects->advisor }}
             </div>
-        </div>
-    </div>
-
-    <div class="jumbotron text-center">
-        <h2>{{ $group_projects->project_title }}</h2>
-
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Team:</strong> 
-                    {{ $group_projects->team }}
-                </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Advisor:</strong> 
-                    {{ $group_projects->advisor }}
-                </div>
-            </div> 
         </div>
     </div>
 </div>
-@endsection
+@endsection 
