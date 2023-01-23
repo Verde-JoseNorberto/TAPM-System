fb3a987abcc28cca168916ba553b9ca8df91d20d
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2023-01-23 08:36:03 +0800

    Created New Controller

---
+++ app/Models/Project.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $table = 'projects';
+    protected $primaryKay = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'file',
+        'description',
+        'comment'
+    ];
+}
