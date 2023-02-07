b9bf0b6228efa2a579563c270c83579368e6c1b3
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2023-02-07 09:41:57 +0800

    Merged Model to one Controller

--- app/Http/Controllers/GroupProjectController.php
+++ app/Http/Controllers/GroupProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use App\Models\Project;
 use App\Models\GroupProject;
 use Illuminate\Http\Request;
@@ -17,15 +18,20 @@ class GroupProjectController extends Controller
     {
         $group_projects = GroupProject::all();
         $projects = Project::all();
+        $tasks = Task::all();
 
         if (auth()->user()->type == 'faculty') {
-            return view('faculty/home', compact(['group_projects', 'projects']));
+            return view('faculty/home', compact(['group_projects']));
+            return view('faculty/project', compact(['group_projects', 'projects', 'tasks']));
         }else if (auth()->user()->type == 'client') {
-            return view('client/home', compact(['group_projects', 'projects']));
+            return view('client/home', compact(['group_projects']));
+            return view('client/project', compact(['group_projects', 'projects', 'tasks']));
         }else if (auth()->user()->type == 'office') {
-            return view('office/home', compact(['group_projects', 'projects']));
+            return view('office/home', compact(['group_projects']));
+            return view('office/project', compact(['group_projects', 'projects', 'tasks']));
         }else{
-            return view('student/home', compact(['group_projects', 'projects']));
+            return view('student/home', compact(['group_projects']));
+            return view('student/project', compact(['group_projects', 'projects', 'tasks']));
         }
     }
     /**
@@ -46,6 +52,17 @@ class GroupProjectController extends Controller
      */
     public function groupStore(Request $request)
     {
+        $request->validate([
+            'project_title'=>'required',
+            'project_category'=>'required',
+            'project_phase'=>'required',
+            'year_term'=>'required',
+            'section'=>'required',
+            'due_date'=>'required',
+            'team'=>'required',
+            'advisor'=>'required',
+        ]);
+
         $add = new GroupProject;
         $add->project_title = $request->project_title;
         $add->project_category = $request->project_category;
@@ -55,19 +72,40 @@ class GroupProjectController extends Controller
         $add->due_date = $request->due_date;
         $add->team = $request->team;
         $add->advisor = $request->advisor;
-        $add->notes = $request->notes;
         $add->save();
         return view('faculty/project')->with('group_projects', $add);
     }
 
     public function projectStore(Request $request)
     {
-        $add = new Project;
-        $add->title = $request->title;
-        $add->file = $request->file;
-        $add->description = $request->description;
-        $add->save();
+        $request->validate([
+            'title'=>'required',
+            'file'=>'required',
+            'description'=>'required',
+        ]);
+
+        $post = new Project;
+        $post->title = $request->title;
+        $post->file = $request->file;
+        $post->description = $request->description;
+        $post->save();
         return view('student/project')->with('projects', $add);
+    }
+
+    public function taskStore(Request $request)
+    {
+        $request->validate([
+            'title'=>'required',
+            'content'=>'required',
+            'due_date'=>'required',
+        ]);
+
+        $tell = new Task;
+        $tell->title = $request->title;
+        $tell->content = $request->content;
+        $tell->due_date = $request->due_date;
+        $tell->save();
+        return view('student/project')->with('tasks', $add);
     }
 
     /**
@@ -76,19 +114,20 @@ class GroupProjectController extends Controller
      * @param  \App\Models\GroupProject  $groupProject
      * @return \Illuminate\Http\Response
      */
-    public function show(GroupProject $group_projects, Project $projects, $id)
+    public function show(GroupProject $group_projects, Project $projects, Task $tasks, $id)
     {
         $group_projects = GroupProject::find($id);
         $projects = Project::find($id);
+        $tasks = Task::find($id);
         
         if (auth()->user()->type == 'faculty') {
-            return view('faculty/project', compact(['group_projects', 'projects']));
+            return view('faculty/project', compact(['group_projects', 'projects', 'tasks']));
         }else if (auth()->user()->type == 'client') {
-            return view('client/project', compact(['group_projects', 'projects']));
+            return view('client/project', compact(['group_projects', 'projects', 'tasks']));
         }else if (auth()->user()->type == 'office') {
-            return view('office/project', compact(['group_projects', 'projects']));
+            return view('office/project', compact(['group_projects', 'projects', 'tasks']));
         }else{
-            return view('student/project', compact(['group_projects', 'projects']));  
+            return view('student/project', compact(['group_projects', 'projects', 'tasks']));
         }
     }
 
@@ -98,9 +137,11 @@ class GroupProjectController extends Controller
      * @param  \App\Models\GroupProject  $groupProject
      * @return \Illuminate\Http\Response
      */
-    public function edit(GroupProject $groupProject)
+    public function edit(GroupProject $group_projects, $id)
     {
-        //
+        $group_projects = GroupProject::find($id);
+        
+        return view('faculty/edit', compact(['group_projects']));
     }
 
     /**
@@ -110,9 +151,33 @@ class GroupProjectController extends Controller
      * @param  \App\Models\GroupProject  $groupProject
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, GroupProject $groupProject)
+    public function update(Request $request, GroupProject $group_projects, $id)
     {
-        //
+        $group_projects = GroupProject::find($id);
+
+        $request->validate([
+            'project_title'=>'required',
+            'project_category'=>'required',
+            'project_phase'=>'required',
+            'year_term'=>'required',
+            'section'=>'required',
+            'due_date'=>'required',
+            'team'=>'required',
+            'advisor'=>'required'
+        ]);
+        
+        $group_projects = GroupProject::find($id);
+
+        $group_projects->project_title = $request->project_title;
+        $group_projects->project_category = $request->project_category;
+        $group_projects->project_phase = $request->project_phase;
+        $group_projects->year_term = $request->year_term;
+        $group_projects->section = $request->section;
+        $group_projects->due_date = $request->due_date;
+        $group_projects->team = $request->team;
+        $group_projects->advisor = $request->advisor;
+        $group_projects->save();
+        return view('faculty/project')->with('group_projects', $group_projects);
     }
 
     /**
@@ -121,13 +186,21 @@ class GroupProjectController extends Controller
      * @param  \App\Models\GroupProject  $groupProject
      * @return \Illuminate\Http\Response
      */
-    public function destroy(GroupProject $group_projects)
+    public function destroy(GroupProject $group_projects, Project $projects, Task $tasks, $id)
     {
-        // $group_projects = GroupProject::find($id);
-        // $group_projects->delete();
+        $group_projects = GroupProject::find($id);
+        $group_projects->delete();
 
-        // // redirect
-        // Session::flash('message', 'Successfully deleted the shark!');
-        // return Redirect::to('/');
+        return Redirect::to('/');
+
+        $projects = Project::find($id);
+        $projects->delete();
+
+        return Redirect::to('/project/{id}');
+
+        $tasks = Tasks::find($id);
+        $tasks->delete();
+
+        return Redirect::to('/project/{id}');
     }
 }
