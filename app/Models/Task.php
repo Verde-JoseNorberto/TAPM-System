2b24fe475260f8a807231dc656ffba906bb94af7
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2023-02-07 09:41:35 +0800

    Added Task Model

---
+++ app/Models/Task.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tasks';
+    protected $primaryKay = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'due_date',
+    ];
+}
