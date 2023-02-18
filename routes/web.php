99e240740b81bdf3899c201af7669f84749cd1d0
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2023-02-18 10:11:03 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GroupProjectController;
-use App\Http\Controllers\GroupController;
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,41 +26,43 @@ Route::get('/', function () {
 })->middleware('auth');
 
 Auth::routes(['verify' => true]);
-Route::middleware(['auth', 'user-access:student'])->group(function () {
-    Route::get('/home', [App\Http\Controllers\UserController::class, 'studentHome'])->name('student/home');
-    Route::get('/home', [App\Http\Controllers\GroupProjectController::class, 'index'])->name('student/home');
+Route::middleware(['auth', 'user-access:office'])->group(function () {
+    Route::get('/office/home', [App\Http\Controllers\UserController::class, 'officeHome'])->name('office/home');
+    Route::get('/office/home', [App\Http\Controllers\GroupProjectController::class, 'index'])->name('office/home');
+    Route::get('/office/project/{id}', [App\Http\Controllers\GroupProjectController::class, 'show']);
+    Route::get('/office/project/{id}/task', [App\Http\Controllers\GroupProjectController::class, 'taskShow']);
+    Route::get('/office/project/{id}/edit', [App\Http\Controllers\GroupProjectController::class, 'edit']);
+    Route::get('/office/admin', [App\Http\Controllers\GroupProjectController::class, 'admin'])->name('office/admin');
 });
 Route::middleware(['auth', 'user-access:faculty'])->group(function () {
     Route::get('/faculty/home', [App\Http\Controllers\UserController::class, 'facultyHome'])->name('faculty/home');
     Route::get('/faculty/home', [App\Http\Controllers\GroupProjectController::class, 'index'])->name('faculty/home');
+    Route::get('/faculty/project/{id}', [App\Http\Controllers\GroupProjectController::class, 'show']);
+    Route::get('/faculty/project/{id}/task', [App\Http\Controllers\GroupProjectController::class, 'taskShow']);
+    Route::get('/faculty/project/{id}/edit', [App\Http\Controllers\GroupProjectController::class, 'edit']);
+
 });
-Route::middleware(['auth', 'user-access:office'])->group(function () {
-    Route::get('/office/home', [App\Http\Controllers\UserController::class, 'officeHome'])->name('office/home');
-    Route::get('/office/home', [App\Http\Controllers\GroupProjectController::class, 'index'])->name('office/home');
+Route::middleware(['auth', 'user-access:student'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\UserController::class, 'studentHome'])->name('student/home');
+    Route::get('/home', [App\Http\Controllers\GroupProjectController::class, 'index'])->name('student/home');
+    Route::get('/project/{id}', [App\Http\Controllers\GroupProjectController::class, 'show']);
+    Route::get('/project/{id}/task', [App\Http\Controllers\GroupProjectController::class, 'taskShow']);
+
 });
 
 Route::controller(GroupProjectController::class)->group(function() {
     Route::post('faculty/home', 'groupStore')->name('faculty/project');
+    Route::post('office/home', 'groupStore')->name('office/project');
+    
     Route::post('/project', 'projectStore')->name('student/project');
-    Route::post('/task', 'taskStore')->name('student/task');
 
-    Route::get('faculty/project/{id}/edit', 'edit')->name('faculty/edit');
-    Route::put('faculty/home', 'update');
+    Route::post('project/task', 'taskStore')->name('student/task');
 
-    Route::get('/project/{id}', 'show');
-    Route::get('/faculty/project/{id}', 'show');
-    Route::get('/office/project/{id}', 'show');
-
-    Route::get('/project/{id}/task', 'taskShow');
-    Route::get('/faculty/project/{id}/task', 'taskShow');
-    Route::get('/office/project/{id}/task', 'taskShow');
-
-
-    Route::delete('faculty/home', 'destroy');
-    Route::delete('student/project', 'destroy');
+    Route::put('faculty/edit/{id}', 'groupUpdate')->name('faclty/edit');
 });
 
-Route::controller(GroupController::class)->group(function() {
-    Route::get('faculty/group', 'index')->name('faculty/group');
-    Route::get('office/group', 'index')->name('office/group');
+Route::controller(CommentController::class)->group(function(){
+    Route::post('faculty/feedback', 'store')->name('faculty/feedback');
+    Route::post('office/project', 'store')->name('office/project');
+    Route::post('/feedback', 'store')->name('student/feedback');
 });
