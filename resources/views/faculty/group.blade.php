1eb338c3091257d53347b2e3b26b23817b9dd56c
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2023-02-18 10:06:44 +0800

    Removed Unnecessary Items

--- resources/views/faculty/group.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.layFac')
-
-@section('page-content')
-<div class="container">
-    <div>
-        
-    </div>
-</div>
-@endsection
--- resources/views/office/group.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.layDir')
-
-@section('page-content')
-<div class="container">
-    <div>
-        
-    </div>
-</div>
-@endsection
