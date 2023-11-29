aecdbb8b7cec10d511b8b390902e0163f486863a
Verde-JoseNorberto <jtverde@student.apc.edu.ph>
2023-11-29 10:06:49 +0800

    Vite Resources Modification

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
-import './calendar';
 import './bootstrap';
-import jQuery from 'jquery';
-import axios from 'axios';
 
-window.$ = jQuery;
-window.axios = axios;
+const successAlert = document.getElementById('success-alert');
+if (successAlert) {
+    setTimeout(function() {
+        successAlert.style.display = 'none';
+    }, 5000);
+}
+
+const errorAlert = document.getElementById('error-alert');
+if (errorAlert) {
+    setTimeout(function() {
+        errorAlert.style.display = 'none';
+    }, 5000);
+}
--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,11 +1,24 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const calendarEl = document.getElementById('calendar');
     const calendar = new Calendar(calendarEl, {
-        plugins: [dayGridPlugin],
-        initialView: 'dayGridMonth'
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth',
+        events: '/calendar/events',
+        eventClick: function (info) {
+            $('#viewEventModal').modal('show');
+            $('#viewEventModalTitle').html(info.event.title);
+            $('#viewEventModalDescription').html(info.event.extendedProps.description);
+            $('#viewEventModalStart').html(info.event.start.toISOString().substring(0, 10));
+            $('#viewEventModalEnd').html(info.event.end.toISOString().substring(0, 10));
+        },
+        dateClick: function (info) {
+            $('#createEventModal').modal('show');
+            $('#createEventModalStart').val(info.dateStr);
+        }
     });
 
     calendar.render();
